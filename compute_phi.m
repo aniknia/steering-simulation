87cea7e7a04bbff1e93d60fcd79071028ec964a5
aniknia <aniknia.dev@gmail.com>
2021-10-04 22:12:35 -0700

    fix: anlges in compute_phi were not raw, anlges produced are now unadultered by tryin to mkae the program compansate for drawing the linkage

--- compute_phi.m
+++ compute_phi.m
@@ -4,5 +4,5 @@ function phi = compute_phi(theta, a, b, cx, cy, h)
         f = (2*b*cy) - (2*a*b*sin(theta));
         g = (2*b*cx) - (2*a*b*cos(theta));
         j = -(a^2 + b^2 + cx^2 + cy^2 - h^2 - (2*a*cx*cos(theta)) - (2*a*cy*sin(theta)));
-        phi = asin((j/sqrt(f^2 + g^2)) + (pi/2)) - atan(g/f) - (pi/2);
+        phi = asin(j/sqrt(f^2 + g^2)) - atan(g/f);
 end
