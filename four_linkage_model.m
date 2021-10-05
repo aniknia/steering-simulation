56f71afc8645c59559f9368de0a948aee661e7c7
aniknia <aniknia.dev@gmail.com>
2021-10-04 22:43:08 -0700

    style: edited code and comments to be more readable

--- four_linkage_model.m
+++ four_linkage_model.m
@@ -1,61 +1,75 @@
 % clears previous figures
 % clear, clc, close all
 
-% values for testing
+% set linkage values for testing
 a = 3;
 b = a;
 cx = 0;
 cy = 6;
 h = 4;
 
-% find angle of straight travel
+% set size of figure
+x_lim = (cy/2) + a + 1;
+y_lim = (cy/2) + a + 1;
+set(gca, 'XLim', [-x_lim, x_lim], 'YLim', [-y_lim, y_lim]);
+
+% set angle values for testing
+z = 0;
+
+% find angles of straight travel
 theta_initial = (pi/2) - acos((cy - h) / (2 * a));
 phi_initial = compute_phi(theta_initial, a, b, cx, cy, h);
 
-% calculated values
-driver_x = 0;
-driver_y = 0;
-passenger_x = 0;
-passenger_y = 0;
+% calculated linkage points on driver and passenger sides
+driver_x = a*cos(theta_initial - (pi/2) + z) - (cy/2);
+driver_y = a*sin(theta_initial - (pi/2) + z);
+passenger_x = a*cos(phi_initial - (pi/2) + z) + (cy/2);
+passenger_y = a*sin(phi_initial - (pi/2) + z);
 
-z = pi/8;
-d = sqrt(((a*cos(theta_initial - (pi/2) + z) - (cy/2))-(a*cos(phi_initial - (pi/2) + z) + (cy/2)))^2 + ((a*sin(theta_initial - (pi/2) + z))-(a*sin(phi_initial - (pi/2) + z)))^2);
-disp(d);
+% calculated of h_linkage from linkage_a and linkage_b (should equal length
+% of h)
+% FIX: issue with length increasing as linkage moves
+d = sqrt((driver_x - passenger_x)^2 + (driver_y - passenger_y)^2);
 
-% draw suspension when traveling straight
+% draw linkages when traveling straight
 hold on;
 plot([-cy/2, cy/2], [0, 0], 'r');
-plot([-cy/2, a*cos(theta_initial - (pi/2) + z) - (cy/2)], [0, a*sin(theta_initial - (pi/2) + z)], 'g');
-plot([cy/2, a*cos(phi_initial - (pi/2) + z) + (cy/2)], [0, a*sin(phi_initial - (pi/2) + z)], 'g');
-plot([a*cos(theta_initial - (pi/2) + z) - (cy/2), a*cos(phi_initial - (pi/2) + z) + (cy/2)], [a*sin(theta_initial - (pi/2) + z), a*sin(phi_initial - (pi/2) + z)], 'b');
+plot([-cy/2, driver_x], [0, driver_y], 'g');
+plot([cy/2, passenger_x], [0, passenger_y], 'g');
+plot([driver_x, passenger_x], [driver_y, passenger_y], 'b');
 
-% % this drew the h linkage, the line above tends to stretch, fix this later
+% % manually draws h_linkage
 % plot([-h/2, h/2], [-sqrt(a^2 - ((cy - h)/2)^2), -sqrt(a^2 - ((cy - h)/2)^2)], 'b');
-
-set(gca, 'XLim', [(-cy/2) - a - 1, (cy/2) + a + 1], 'YLim', [(-cy/2) - a - 1, (cy/2) + a + 1]);
 hold off;
 
-% possible thetas (change this to take into account tire width and length, fix later)
+% possible thetas
+% FEAT: must take into account tire width and length
 theta_lin = linspace(-pi/4 , pi/4, 100);
+
+% set arrays for recording driver and passenger sed linkage angles
 test_array_theta = ones(1, 100);
 test_array_phi = ones(1, 100);
 j = 0;
 
 for i = theta_lin
+    % reset figure
     cla reset;
-    hold on;
-    set(gca, 'XLim', [(-cy/2) - a - 1, (cy/2) + a + 1], 'YLim', [(-cy/2) - a - 1, (cy/2) + a + 1]);
     
+    % set size of figure
+    set(gca, 'XLim', [-x_lim, x_lim], 'YLim', [-y_lim, y_lim]);
+    
+    % records driver and passenger side linkage angles
     j = j + 1;
     phi = compute_phi(i, a, b, cx, cy, h);
     test_array_theta(j) = test_array_theta(j) * rad2deg(i);
     test_array_phi(j) = test_array_phi(j) * rad2deg(phi);
     
+    % draws linkages
+    hold on;
     plot([-cy/2, cy/2], [0, 0], 'r');
     plot([-cy/2, a*cos(i - (pi/2) + z) - (cy/2)], [0, a*sin(i - (pi/2) + z)], 'g');
     plot([cy/2, a*cos(phi - (pi/2) + z) + (cy/2)], [0, a*sin(phi - (pi/2) + z)], 'g');
     plot([a*cos(i - (pi/2) + z) - (cy/2), a*cos(phi - (pi/2) + z) + (cy/2)], [a*sin(i - (pi/2) + z), a*sin(phi - (pi/2) + z)], 'b');
-    
     hold off;
     drawnow;
 end
