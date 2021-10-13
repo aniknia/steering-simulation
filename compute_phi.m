7106088475526be7e8833e51098c2693c12490a7
aniknia <aniknia.dev@gmail.com>
2021-10-13 00:14:35 -0700

    refactor: made compute_phi easier to call by using array of linkage lengths

--- compute_phi.m
+++ compute_phi.m
@@ -1,8 +1,9 @@
-function phi = compute_phi(theta, a, b, cx, cy, h)
+function phi = compute_phi(theta, lengths)
 % Outputs (phi) angle of the right wheel based on (theta) angle of the left
 % wheel as well as given constants
-        f = (2*b*cy) - (2*a*b*sin(theta));
-        g = (2*b*cx) - (2*a*b*cos(theta));
-        j = -(a^2 + b^2 + cx^2 + cy^2 - h^2 - (2*a*cx*cos(theta)) - (2*a*cy*sin(theta)));
+% lengths = [a, b, cx, cy, h];
+        f = ( 2 * lengths(2) * lengths(4) ) - ( 2 * lengths(1) * lengths(2) * sin(theta));
+        g = ( 2 * lengths(2) * lengths(3) ) - ( 2 * lengths(1) * lengths(2) * cos(theta));
+        j = -( lengths(1)^2 + lengths(2)^2 + lengths(3)^2 + lengths(4)^2 - lengths(5)^2 - ( 2 * lengths(1) * lengths(3) * cos(theta)) - ( 2 * lengths(1) * lengths(4) * sin(theta)));
         phi = asin(j/sqrt(f^2 + g^2)) - atan(g/f);
 end
