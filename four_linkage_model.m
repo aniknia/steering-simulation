3f629ff69e539cc23d47e2aa7c020c34e2cc3fed
aniknia <aniknia.dev@gmail.com>
2021-10-26 03:58:05 -0700

    refactor: functions now work with matlab apps

--- four_linkage_model.m
+++ four_linkage_model.m
@@ -1,39 +1,40 @@
-% clears previous figures
-% clear, clc, close all
+function four_linkage_model(a, b, cx, cy, h, varargin)
+    if nargin == 0
+        [a, b, cx, cy, h] = deal(1.5, 1.5, 3, 0, 1);
+    end
+    % set linkage values for testing
+    lengths = [a, b, cx, cy, h];
 
-% set linkage values for testing
-[a, b, cx, cy, h] = deal(1.5, 1.5, 3, 0, 1);
-lengths = [a, b, cx, cy, h];
+    % find angles of straight travel and max and min
+    % theta_initial, phi_initial, theta_max, theta_min
+    angle_init_max_arr = angle_init_max(lengths);
 
-% find angles of straight travel
-theta_initial = acos((cx - h) / (2 * a));
-phi_initial = compute_phi(theta_initial, lengths);
+    % draw linkages when traveling straight
+    %set_plot_const(lengths)
+    %plot_four_bar_linkage(compute_linkage_points(angle_init_max_arr(1), angle_init_max_arr(2:3), lengths));
 
-% find max and min angles
-theta_max = acos((a^2 + cx^2 - (b + h)^2) / (2 * a * cx));
-theta_min = asin( a / ((b + h) / sin(theta_max)));
+    % possible thetas
+    % FEAT: must take into account tire width and length
+    n = 100;
+    theta_lin = linspace(angle_init_max_arr(5) , angle_init_max_arr(4), n);
 
-% draw linkages when traveling straight
-set_plot_const(lengths)
-plot_four_bar_linkage(compute_linkage_points(theta_initial, phi_initial, lengths));
+    % set arrays for recording driver and passenger sed linkage angles
+    [test_array_theta, test_array_phi] = deal(ones(1, n));
+    j = 0;
 
-% possible thetas
-% FEAT: must take into account tire width and length
-n = 100;
-theta_lin = linspace(theta_min , theta_max, n);
+    for i = theta_lin
+        % draws linkages
+        % set_plot_const(lengths)
+        phi = compute_phi(i, lengths);
+        if nargin == 6
+            app = varargin{1};
+            plot_four_bar_linkage(compute_linkage_points(i, phi, lengths), app);
+        else
+            plot_four_bar_linkage(compute_linkage_points(i, phi, lengths));
+        end
 
-% set arrays for recording driver and passenger sed linkage angles
-[test_array_theta, test_array_phi] = deal(ones(1, n));
-j = 0;
-
-for i = theta_lin
-    
-    % records driver and passenger side linkage angles
-    j = j + 1;
-    phi = compute_phi(i, lengths);
-    [test_array_theta(j), test_array_phi(j)] = deal(test_array_theta(j) * i, test_array_phi(j) * phi(2));
-    
-    % draws linkages
-    set_plot_const(lengths)
-    plot_four_bar_linkage(compute_linkage_points(i, phi, lengths));
+        % records driver and passenger side linkage angles
+        j = j + 1;
+        [test_array_theta(j), test_array_phi(j)] = deal(test_array_theta(j) * i, test_array_phi(j) * phi(2));
+    end
 end
--- plot_four_bar_linkage.m
+++ plot_four_bar_linkage.m
@@ -1,9 +1,20 @@
-function plot_four_bar_linkage(linkage_points)
+function plot_four_bar_linkage(linkage_points, varargin)
 % Outputs nothing. Draws linkages from points
-        hold on;
-        plot([linkage_points(1), linkage_points(2)], [linkage_points(3), linkage_points(4)], 'r');
-        plot([linkage_points(5), linkage_points(6)], [linkage_points(7), linkage_points(8)], 'g');
-        plot([linkage_points(9), linkage_points(10)], [linkage_points(11), linkage_points(12)], 'g');
-        plot([linkage_points(13), linkage_points(14)], [linkage_points(15), linkage_points(16)], 'b');
-        hold off;
-        drawnow;
+        if nargin == 2
+            app = varargin{1};
+            hold(app, 'on');
+            plot(app, [linkage_points(1), linkage_points(2)], [linkage_points(3), linkage_points(4)], 'r');
+            plot(app, [linkage_points(5), linkage_points(6)], [linkage_points(7), linkage_points(8)], 'g');
+            plot(app, [linkage_points(9), linkage_points(10)], [linkage_points(11), linkage_points(12)], 'g');
+            plot(app, [linkage_points(13), linkage_points(14)], [linkage_points(15), linkage_points(16)], 'b');
+            hold(app, 'off');
+            drawnow;
+        else
+            hold on;
+            plot([linkage_points(1), linkage_points(2)], [linkage_points(3), linkage_points(4)], 'r');
+            plot([linkage_points(5), linkage_points(6)], [linkage_points(7), linkage_points(8)], 'g');
+            plot([linkage_points(9), linkage_points(10)], [linkage_points(11), linkage_points(12)], 'g');
+            plot([linkage_points(13), linkage_points(14)], [linkage_points(15), linkage_points(16)], 'b');
+            hold off;
+            drawnow;
+        end
